01981ca2663a51d0be970c914862af68b23ab053
david.llarena <dasanbas@gmail.com>
2014-08-15 13:56:02 +0200

    First Commit

---
+++ src/com/prueba/Prueba.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.prueba;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Prueba {
+
+	/**
+	 * 
+	 */
+	public Prueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
